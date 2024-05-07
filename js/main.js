c2fa40dba63cdce09ca143311c42d45fdc91f93f
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-05-07 13:36:49 +0100

    Added Full screen capability

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// const requestFullScreen = document.fullscreenEnabled
+// console.log(requestFullScreen)
+
+let fullScreenState = false;
+
+const toggleFullScreen = () => {
+  {
+    fullScreenState
+      ? document.documentElement.requestFullscreen()
+      : document.exitFullscreen();
+  }
+
+  fullScreenState = !fullScreenState;
+};
+
+document.addEventListener("dblclick", () => {
+  toggleFullScreen();
+});
+})();
